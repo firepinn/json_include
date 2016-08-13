9cc60d2533c184a2d15e6c43eac675a0d6293498
Arya Mazaheri <aryanet@gmail.com>
2016-08-13 22:16:17 +0200

    adjust json include

--- json_include.py
+++ json_include.py
@@ -9,10 +9,16 @@ from collections import OrderedDict
 import argparse
 
 OBJECT_TYPES = (dict, list)
-INCLUDE_KEY = '...'
-INCLUDE_VALUE_PATTERN = re.compile(r'^include\((.+)\)$')
+INCLUDE_KEYS = ['...', '$ref']
+INCLUDE_VALUE_PATTERNS = [
+    re.compile(r'^#/(.+)$'),                # simple local definition
+    re.compile(r'^include\((.+)\)$'),       # include
+    re.compile(r'^file:(.+)?#/(.+)$'),      # remote definition inclusion
+    re.compile(r'^file:(.+)$'),             # remote file inclusion
+]
 INCLUDE_TEXT_PATTERN = re.compile(r'^include_text\((.+)\)$')
 _included_cache = {}
+_original_schemas = []
 
 
 def random_string(N=9):
@@ -24,12 +30,19 @@ def read_file(filepath):
         return f.read()
 
 
-def get_include_name(value, regex):
-    if isinstance(value, basestring):
-        rv = regex.search(value)
-        if rv:
-            return rv.groups()[0]
-    return None
+def get_include_name(value, regex_list):
+    for idx, regex in enumerate(regex_list):
+        if isinstance(value, basestring):
+            rv = regex.search(value)
+            if rv:
+                return rv.groups(), idx
+    return None, None
+
+
+def lookup(dic, key, *keys):
+    if keys:
+        return lookup(dic.get(key, {}), *keys)
+    return dic.get(key)
 
 
 def make_unique(obj, key, original=None, replacement=None):
@@ -49,23 +62,47 @@ def make_unique(obj, key, original=None, replacement=None):
     return obj
 
 
+def include_definition(include_name, schema):
+    attr = include_name.split("/")
+    return lookup(schema, *attr)
+
+
+def include_remote_file(dirpath, include_name):
+    _f = os.path.join(dirpath, include_name)
+    if include_name not in _included_cache:
+        remote_schema = parse_json_include(os.path.dirname(_f), os.path.basename(_f), True)
+        remote_schema.pop('$schema', None)  # remove $schema property before inclusion
+        remote_schema.pop('title', None)    # remove title property before inclusion
+        return remote_schema
+    else:
+        return _included_cache[include_name]
+
+
 def walk_through_to_include(o, dirpath):
     if isinstance(o, dict):
         is_include_exp = False
         make_unique_key = o.pop('makeUnique', None)
-        if set(o) == set([INCLUDE_KEY]):
-            include_name = get_include_name(o.values()[0], INCLUDE_VALUE_PATTERN)
-            if include_name:
+        if INCLUDE_KEYS[0] in o or INCLUDE_KEYS[1] in o:
+            include_key = INCLUDE_KEYS[0] if INCLUDE_KEYS[0] in o else INCLUDE_KEYS[1]
+            include_info, include_idx = get_include_name(o[include_key], INCLUDE_VALUE_PATTERNS)
+            if include_info:
                 is_include_exp = True
-                o.clear()
-                # enable relative directory references: `../../`
-                _f = os.path.join(dirpath, include_name)
-                if include_name not in _included_cache:
-                    _included_cache[include_name] = parse_json_include(
-                        os.path.dirname(_f), os.path.basename(_f), True)
+                include_name = include_info[0]
+                if include_idx == 0:
+                    # include local definitions
+                    _included_cache[include_name] = include_definition(include_name, _original_schemas[-1])
+                elif include_idx == 2:
+                    # include remote definitions
+                    include_name = include_info[1]
+                    remote_file_schema = include_remote_file(dirpath, include_info[0])
+                    _included_cache[include_name] = include_definition(include_name, remote_file_schema)
+                else:
+                    # enable relative directory references: `../../`
+                    _included_cache[include_name] = include_remote_file(dirpath, include_name)
+
+                o.pop(include_key)
                 _data = _included_cache[include_name]
                 o.update(make_unique(_data, make_unique_key) if make_unique_key else _data)
-
         include_text_keys = [key for key in o.keys()
                              if isinstance(o[key], basestring) and INCLUDE_TEXT_PATTERN.search(o[key])]
         for key in include_text_keys:
@@ -74,10 +111,8 @@ def walk_through_to_include(o, dirpath):
                 _f = os.path.join(dirpath, include_filename)
                 with open(os.path.join(_f)) as file:
                     o[key] = file.read()
-
         if is_include_exp:
             return
-
         for k, v in o.iteritems():
             if isinstance(v, OBJECT_TYPES):
                 walk_through_to_include(v, dirpath)
@@ -90,14 +125,15 @@ def walk_through_to_include(o, dirpath):
 def parse_json_include(dirpath, filename, is_include=False):
     filepath = os.path.join(dirpath, filename)
     json_str = read_file(filepath)
-    # d = json.loads(json_str, object_pairs_hook=OrderedDict)
     d = resolve_extend_replace(json_str, filepath)
+    _original_schemas.append(d)
 
     if is_include:
         assert isinstance(d, dict),\
             'The JSON file being included should always be a dict rather than a list'
 
     walk_through_to_include(d, dirpath)
+    _original_schemas.pop()
 
     return d
 
@@ -161,31 +197,28 @@ def resolve_extend_replace(str, filepath):
     :rtype: dict
     """
     obj = json.loads(str, object_pairs_hook=OrderedDict)
-    if not isinstance(obj, dict):
-        return obj
     extend = obj.get("$extend", {})
     replace = obj.get("$replace", {})
     filename = extend.get("name", None)
     if filename:
         json_string = read_file(os.path.join(os.path.dirname(filepath), filename))
         json_data = json.loads(json_string, object_pairs_hook=OrderedDict)
+        idx_cache = 0
         for entry in replace:
             key = entry["where"]["key"]
             idx = entry["where"].get("idx", None)
-            idx_cache = 0
-            _with = entry["with"]
-            _replacement = obj.get(_with.replace("$this.", "")) if _with and "$this." in _with else _with
-            _current_value = json_data[key]
-            if (idx or idx == 0) and isinstance(_current_value, list):
-                del _current_value[idx]
-                if isinstance(_replacement, list):
-                    for _in, _el in enumerate(_replacement):
-                        _current_value.insert(idx + _in, _el)
+            idx = idx + idx_cache if idx else None
+            _d = entry["with"]
+            _repl = obj.get(_d.replace("$this.", "")) if _d and "$this." in _d else _d
+            _value = json_data[key]
+            if idx and isinstance(_value, list):
+                del _value[idx]
+                if isinstance(_repl, list):
+                    for _in, _el in enumerate(_repl):
+                        _value.insert(idx + _in, _el)
                         idx_cache += 1
-                else:
-                    _current_value.insert(idx, _replacement)
-                _replacement = _current_value
-            json_data[key] = _replacement
+                _repl = _value
+            json_data[key] = _repl
         obj = json_data
     return obj
 
