2636269b858281c68d0356178a5742f9d19978a1
Mohammad Mohammadi <mohammad@exabyte.io>
2017-07-27 17:04:32 -0700

    refactored json_include

--- setup.py
+++ setup.py
@@ -1,48 +1,17 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
 from setuptools import setup
 
-
-# Use semantic versioning: MAJOR.MINOR.PATCH
-version = '0.2.9'
-
-
-def get_requires():
-    with open('requirements.txt', 'r') as f:
-        requires = [i for i in map(lambda x: x.strip(), f.readlines()) if i]
-    return requires
-
-
-def get_long_description():
-    with open('README.rst', 'r') as f:
-        return f.read()
-
-
 setup(
-    name='json-include',
-    version=version,
-    author='reorx',
-    author_email='timur.bazhirov@gmail.com',
-    url='https://github.com/timurbazhirov/json_include',
+    name='jsonInclude',
+    version='0.2.9',
     description='An extension for JSON to support file including',
-    long_description=get_long_description(),
-    py_modules=[
-        'json_include',
-    ],
-    # Or use (make sure find_packages is imported from setuptools):
-    # packages=find_packages()
-    # install_requires=get_requires(),
-    # package_data={}
-    entry_points={
-        'console_scripts': [
-            'json-include=json_include:main'
-        ]
-    },
-    license='MIT',
+    url='https://github.com/Exabyte-io/json_include',
+    author='Exabyte Inc.',
+    author_email='info@exabyte.io',
+    packages=["JsonInclude"],
     classifiers=[
-        'License :: OSI Approved :: MIT License',
         'Programming Language :: Python',
-        'Programming Language :: Python :: 2.7',
-    ],
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development'
+    ]
 )
