f74d299920ab2f11c35db21dc01f792199381d6b
Mohammad Mohammadi <mohammad@exabyte.io>
2017-07-27 17:10:57 -0700

    fixed package name

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     url='https://github.com/Exabyte-io/json_include',
     author='Exabyte Inc.',
     author_email='info@exabyte.io',
-    packages=["JsonInclude"],
+    packages=["jsonInclude"],
     classifiers=[
         'Programming Language :: Python',
         'Development Status :: 3 - Alpha',
