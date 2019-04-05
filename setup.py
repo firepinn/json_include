ed6e22fb2f73f2eaa090c125169f832efcb10f7c
Mohammad Mohammadi <mohammad@exabyte.io>
2019-04-05 12:51:53 -0700

    Add support for list

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='exabyte_json_include',
-    version='0.1.1',
+    version='2.0.0',
     description='An extension for json_include to support file inclusion',
     url='https://github.com/Exabyte-io/json_include',
     author='Exabyte Inc.',
